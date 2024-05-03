a3920eed647e190c91ea9b5db5bcf016832646bf
Javier Bullrich <javier@bullrich.dev>
2024-05-03 22:12:57 +0200

    fixed lack of async operator

--- scripts/content-creator.js
+++ scripts/content-creator.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const StoryGenerator = require("./story-generator");
 const core = require('@actions/core');
 
+const USER_NAME = process.env.USER_NAME;
+const AVATAR = process.env.USER_AVATAR;
 const key = process.env.OPEN_AI_KEY;
 if (!key) {
     throw new Error("Missing key OPEN_AI_KEY");
@@ -10,11 +12,16 @@ if (!key) {
 const generator = new StoryGenerator(key);
 
 // We have the prompt to read available in a text file
-fs.readFile('./prompt.txt', 'utf8', function (err, data) {
+fs.readFile('./prompt.txt', 'utf8', async function (err, data) {
     if (err) {
         throw err;
     }
+    let helper;
+    if (USER_NAME && AVATAR) {
+        helper = { name: USER_NAME, avatar: AVATAR }
+    }
+
     // We create the blog post with the data of the prompt
-    const story = generator.generateBlogPost(data);
-    core.setOutput('title', story.title);
+    const story = await generator.generateBlogPost(data, helper);
+    core.setOutput('title', story.title.replace(/\"/g, ""));
 });
