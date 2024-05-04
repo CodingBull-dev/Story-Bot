d9b08d5010385e51746a53dd89a070b8a088f008
Javier Bullrich <javier@bullrich.dev>
2024-05-04 18:05:24 +0200

    upgraded code to use new library

--- scripts/action.js
+++ scripts/action.js
@@ -1,32 +1,31 @@
-const fs = require('fs');
-const StoryGenerator = require("./story-generator");
-const core = require('@actions/core');
+import { readFile } from 'fs';
+import { writeFiles, today } from "./story-generator.js";
+import { setOutput } from '@actions/core';
 
 const USER_NAME = process.env.USER_NAME;
 const AVATAR = process.env.USER_AVATAR;
 
 function setOutputs(fileName, story) {
-    core.setOutput('file', fileName);
-    core.setOutput('url', fileName.replace(/^.*[\\\/]/, '').replace(".md", ""));
-    core.setOutput('title', story.title);
+    setOutput('file', fileName);
+    setOutput('url', fileName.replace(/^.*[\\\/]/, '').replace(".md", ""));
+    setOutput('title', story.title);
 }
 
 async function generateStoryFromIssue() {
-    const generator = new StoryGenerator(null);
     let helper;
     if (USER_NAME && AVATAR) {
         helper = { name: USER_NAME, avatar: AVATAR }
     }
     // We have the prompt to read available in a text file
-    fs.readFile('./story.json', 'utf8', function (err, data) {
+    readFile('./story.json', 'utf8', function (err, data) {
         if (err) {
             throw err;
         }
 
         const story = JSON.parse(data);
 
-        generator.writeFiles(story, story.image, helper).then(file => {
-            setOutputs(file.replace(`${generator.today}-`, ""), story);
+        writeFiles(story, story.image, helper).then(file => {
+            setOutputs(file.replace(`${today}-`, ""), story);
         });
     });
 }
--- scripts/story-generator.js
+++ scripts/story-generator.js
@@ -1,179 +1,67 @@
-const { Configuration, OpenAIApi } = require("openai");
-const fs = require('fs');
-var string = require("string-sanitizer");
-const yaml = require("json2yaml");
-const Image = require("@11ty/eleventy-img");
-const moment = require("moment");
+import fs from 'fs';
+import string from 'string-sanitizer';
+import yaml from 'json2yaml';
+import Image from '@11ty/eleventy-img';
+import moment from 'moment';
 
-class StoryGenerator {
-    constructor(apiKey) {
-        const configuration = new Configuration({
-            apiKey
-        });
 
-        this.openai = new OpenAIApi(configuration);
+// generate today's date to order files
+export const today = moment().format("YYYY-MM-DD");
 
-        // generate today's date to order files
-        this.today = moment().format("YYYY-MM-DD");
+
+export async function writeFiles(story, imgUrl, helper) {
+    console.log("Writing file for the following object", story);
+    const imageName = `${today}-${string.sanitize.addDash(story.title)}`.toLowerCase();
+    const stats = await Image(imgUrl, {
+        formats: ["webp"],
+        outputDir: "./src/img/blog/",
+        filenameFormat: (id, src, width, format, options) => {
+            return `${imageName}.${format}`;
+        }
+    });
+    console.log("Finished saving image", stats);
+    const img = stats.webp[0].filename;
+
+    console.log("Generating markdown file");
+
+    const data = {
+        layout: "layouts/blog.html",
+        title: story.title,
+        date: moment().toISOString(),
+        categories: "blog",
+        tags: 'gpt',
+        prompt: story.prompt,
+        generation: {
+            temperature: story.temperature
+        }
+    };
+    if (img) {
+        data.image = img;
+    }
+    if (helper) {
+        data.generation.helper = { name: helper.name, avatar: helper.avatar };
     }
 
-    async generateStory(prompt) {
-        const story = {};
-
-        const temperature = Number(Math.random().toFixed(2));
-
-        console.log("Requesting story.", "Temperature for the story is:", temperature);
-
-        const systemInfo = `You are Story Bot, a language model that helps users create stories, scripts and more. 
-        Follow the user's instructions carefully and generate the content they requested.
-        When writing a post, story or script, try to extend the text as much as possible without making it boring.`
-
-        const storyMessages = [
-            { "role": "system", "content": systemInfo },
-            { "role": "user", "content": prompt }
-        ];
-        const response = await this.openai.createChatCompletion({
-            model: "gpt-4-1106-preview",
-            temperature: temperature,
-            messages: storyMessages
-        });
-
-        story.prompt = prompt;
-        story.content = response.data.choices[0].message.content;
-        story.temperature = temperature;
-
-        console.log("Got the story!", `It is ${story.content.split(" ").length} words long!`);
-
-        console.log("Requesting story title");
-
-        const titleQuestion = await this.openai.createChatCompletion({
-            model: "gpt-4-1106-preview",
-            messages: [
-                ...storyMessages,
-                response.data.choices[0].message,
-                { "role": "user", "content": "What would you call the story (or post)? Respond only with the name, no other text is needed." }
-            ]
-        });
-
-        let title = titleQuestion.data.choices[0].message.content
-
-        // Remove dots at the end of the title
-        if (title[title.length - 1] === ".") {
-            title = title.slice(0, -1);
-        }
-
-        story.title = title;
-
-        console.log("Got the story title:", story.title);
-
-        const imgPrompt = `Based on the previous story, write a prompt for an image generation service. It is intended to be the cover of the blog post.
-Respond only with the prompt. No other text is needed. Keep the prompt short`
-
-        const promptQuestion = await this.openai.createChatCompletion({
-            model: "gpt-4-1106-preview",
-            messages: [
-                ...storyMessages,
-                response.data.choices[0].message,
-                { "role": "user", "content": "What would you call the story? Respond only with the name, no other text is needed." },
-                titleQuestion.data.choices[0].message,
-                { "role": "user", "content": imgPrompt }
-            ]
-        });
-
-        const imagePrompt = promptQuestion.data.choices[0].message.content;
-
-        story.imagePrompt = imagePrompt;
-
-        return story;
-    }
-
-    async generateStoryPhoto(story) {
-        console.log('Generating image for the story with the following prompt:', story.imagePrompt);
-
-        const response = await this.openai.createImage({
-            prompt: story.imagePrompt,
-            n: 1,
-            size: "512x512",
-        });
-
-        console.log('Got the image.', 'Downloading file now!');
-
-        return response.data.data[0].url;
-    }
-
-    async writeFiles(story, imgUrl, helper) {
-        const imageName = `${this.today}-${string.sanitize.addDash(story.title)}`.toLowerCase();
-        const stats = await Image(imgUrl, {
-            formats: ["webp"],
-            outputDir: "./src/img/blog/",
-            filenameFormat: (id, src, width, format, options) => {
-                return `${imageName}.${format}`;
-            }
-        });
-        console.log("Finished saving image", stats);
-        const img = stats.webp[0].filename;
-
-        console.log("Generating markdown file");
-
-        const data = {
-            layout: "layouts/blog.html",
-            title: story.title,
-            date: moment().toISOString(),
-            categories: "blog",
-            tags: 'gpt',
-            prompt: story.prompt,
-            generation: {
-                temperature: story.temperature
-            }
-        };
-        if (img) {
-            data.image = img;
-        }
-        if (helper) {
-            data.generation.helper = { name: helper.name, avatar: helper.avatar };
-        }
-
-        const ymlData = yaml.stringify(data).trim();
+    const ymlData = yaml.stringify(data).trim();
 
 
-        console.log("Configuration for the markdown file\n", ymlData);
+    console.log("Configuration for the markdown file\n", ymlData);
 
-        const markdown = `${ymlData}
+    const markdown = `${ymlData}
 ---
 ${story.content}
 `;
 
-        const fileName = `./src/blog/${this.today}-${string.sanitize.addDash(story.title)}.md`.toLowerCase();
-        return new Promise((res, rej) => {
-            fs.writeFile(fileName, markdown, err => {
-                if (err) {
-                    console.error(err);
-                    rej(err);
-                } else {
-                    console.log(`Finished writing to ${fileName}`);
-                    res(fileName);
-                }
-            });
-        });
-    }
-
-    async generateBlogPost(prompt, helper) {
-        try {
-            const story = await this.generateStory(prompt);
-            const photo = await this.generateStoryPhoto(story);
-            const file = await this.writeFiles(story, photo, helper);
-            return story;
-        }
-        catch (error) {
-            if (error.response) {
-                console.error(error.response.status);
-                console.error(error.response.data);
+    const fileName = `./src/blog/${today}-${string.sanitize.addDash(story.title)}.md`.toLowerCase();
+    return new Promise((res, rej) => {
+        fs.writeFile(fileName, markdown, err => {
+            if (err) {
+                console.error(err);
+                rej(err);
             } else {
-                console.error(error);
+                console.log(`Finished writing to ${fileName}`);
+                res(fileName);
             }
-            throw new Error("Failed during the operation");
-        }
-    }
+        });
+    });
 }
-
-module.exports = StoryGenerator;
